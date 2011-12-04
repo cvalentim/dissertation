8948b4b92f440afc1eda2cead333f5254e11f494
Caio Valentim <kakaio9@gmail.com>
2011-12-04 20:02:46 -0200

    making the command line options obrigatory. That helps to keep experiments records.

--- imp/main.cpp
+++ imp/main.cpp
@@ -14,11 +14,29 @@
 
 using namespace std;
 
+void help(){
+	cout<<"./<program> <REAL_DATASET | RANDOM_DATASET> <FPAIR_H | RMQ_H>"<<endl;
+	cout<<"REAL_DATASET -> It uses the real instances available on /instances/real_data"<<endl;
+	cout<<"RANDOM_DATASET -> It uses random instances available on /instances/random"<<endl;
+	cout<<"FPAIR -> It uses the FPairs heuristic to answer the queries"<<endl;
+	cout<<"RMQ_H -> It uses the RMQ_H heuristic to answer the queries"<<endl;
+	exit(0);
+}
+
 int main(int argc, char* argv[])
 {
 	bool use_rdataset = false, use_rmqH = false;
-	if (argc > 1 && strcmp(argv[1], "RANDOM_DATASET") == 0)	use_rdataset = true;
-	if (argc > 2 && strcmp(argv[2], "RMQ_H") == 0) use_rmqH = true;
+	if (argc < 2) {
+			help();
+	}
+
+	if (strcmp(argv[1], "RANDOM_DATASET") == 0)	use_rdataset = true;
+	else if (strcmp(argv[1], "REAL_DATASET") == 0) use_rdataset = false;
+	else help();
+	
+	if (strcmp(argv[2], "RMQ_H") == 0) use_rmqH = true;
+	else if (strcmp(argv[2], "FPAIR_H") == 0) use_rmqH = false;
+	else help();
 
 	if (use_rdataset){
 			DataHandler<int> *data_set = new RandomDataHandler<int>();
