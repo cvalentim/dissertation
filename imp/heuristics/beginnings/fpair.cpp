6520b0447b333e7684b3010318a1301315e4ff24
Caio Valentim <kakaio9@gmail.com>
2012-01-17 21:10:10 -0200

    Making fpair generic using templates

--- imp/heuristics/beginnings/fpair.cpp
+++ imp/heuristics/beginnings/fpair.cpp
@@ -8,9 +8,10 @@
 using namespace std;
 
 
+template<class T>
 struct FPair{
 		int s, e;	
-		double vs, ve;
+		T vs, ve;
 		
 		FPair(){} 
 
@@ -19,7 +20,7 @@ struct FPair{
 				e = _e;
 		}
 
-		FPair(int _s, int _e, double _vs, double _ve){
+		FPair(int _s, int _e, T _vs, T _ve){
 			s = _s, e = _e, vs = _vs, ve = _ve;
 		}
 
@@ -27,31 +28,29 @@ struct FPair{
 				return e - s;
 		}
 	
-		double get_delta_v() const{
+		T get_delta_v() const{
 				return ve - vs;
 		}
 		
-		bool operator==(const FPair& rhs) const{
+		bool operator==(const FPair<T>& rhs) const{
 				return s == rhs.s && e == rhs.e;
 		}
 		// less than operator ?
 		
-		friend ostream& operator<<(ostream& out, const FPair& x ) 
+		friend ostream& operator<<(ostream& out, const FPair<T>& x ) 
 		{
 			out << "(" << x.s << ", " << x.e << ", " << x.vs <<", "<< x.ve <<")"<<endl;
 			return out;
 		}
 };
 
-// FIXME: Make the input vector generic, possibly using
-// templates
 template<typename T>
-void construct(int s, int e, vector<T>& A, RMQNaive<T>& rmq_max, RMQNaive<T>& rmq_min, vector<FPair>& res ){
+void construct(int s, int e, vector<T>& A, RMQNaive<T>& rmq_max, RMQNaive<T>& rmq_min, vector<FPair<T> >& res ){
 		if (s > e) return;
 		int hi = rmq_max.query(s, e, greater<T>());
 		int lo = rmq_min.query(s, hi);
 		while (lo < hi && A[lo] < A[hi]){
-			res.push_back(FPair(lo, hi, A[lo], A[hi]));
+			res.push_back(FPair<T>(lo, hi, A[lo], A[hi]));
 			lo = rmq_min.query(lo + 1, hi);		
 		}
 		construct(s, hi - 1, A, rmq_max, rmq_min, res);
@@ -59,24 +58,14 @@ void construct(int s, int e, vector<T>& A, RMQNaive<T>& rmq_max, RMQNaive<T>& rm
 }
 
 template<typename T>
-vector<FPair> find_fpairs(vector<T>& A){
-	vector<FPair> res;
+vector<FPair<T> > find_fpairs(vector<T>& A){
+	vector<FPair<T> > res;
 	RMQNaive<T> rmq_max, rmq_min;
 
 	rmq_max.preprocess(A, greater<T>());
 	rmq_min.preprocess(A);
 
 	construct(0, A.size() - 1, A, rmq_max, rmq_min, res);
-
-/*	for (int i = 0; i < A.size(); ++i){
-		int smallest = i, greatest = i;
-		for (int j = i + 1; j < A.size(); ++j){
-				if (A[j] < A[smallest]) smallest = j;
-				if (A[j] > A[greatest]) greatest = j;
-				if (A[greatest] == A[j] && A[smallest] == A[i])
-						 res.push_back(FPair(i, j, A[i], A[j]));
-		}
-	}*/
 	return res;
 }
 
