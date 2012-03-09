f8c9a19aa08e7847257f90312d8b7df1f93ae17e
Caio Valentim <kakaio9@gmail.com>
2012-03-09 17:09:51 -0300

    Making main for flexible for executing different types of heuristics.

--- imp/main.cpp
+++ imp/main.cpp
@@ -9,66 +9,68 @@
 
 //#include "heuristics/pure_rmq.cpp"
 //#include "heuristics/fpairs.cpp"
-#include "heuristics/beginnings/range_list.cpp"
+#include "heuristics/beginnings/range_list/range_list.cpp"
 #include "heuristics/beginnings/rmq_based.cpp"
+#include "heuristics/beginnings/allpairs_fpairs.cpp"
 #include "heuristics/heuristic.cpp"
 #include "data_set_handler.cpp"
 #include "executer.cpp"
 
 using namespace std;
 
+#define RMQ_BUCKET 0
+#define FPAIR 1
+#define ALLPAIRS_FPAIR 2
+#define RMQ_ST 3
+
 void help(){
-	cout<<"./<program> <REAL_DATASET | RANDOM_DATASET> <FPAIR_H | RMQ_H>"<<endl;
-	cout<<"REAL_DATASET -> It uses the real instances available on /instances/real_data"<<endl;
-	cout<<"RANDOM_DATASET -> It uses random instances available on /instances/random"<<endl;
+	cout<<"./<program> <OUTSIZE_SMALL | OUTSIZE_LARGE> <RMQ_BUCKET | FPAIR | ALLPAIRS_FPAIR | RMQ_ST>"<<endl;
+	cout<<"<OUTSIZE_SMALL | OUTSIZE_LARGE > -> controls the size of query answers"<<endl;
+	cout<<"RMQ_BUCKET -> Uses the RMQ based heuristic with the RMQBucket implementation for the RMQ."<<endl;
 	cout<<"FPAIR -> It uses the FPairs heuristic to answer the queries"<<endl;
-	cout<<"RMQ_H -> It uses the RMQ_H heuristic to answer the queries"<<endl;
+	cout<<"RMQ_ST -> it uses the RMQ based heuristic backed by the <O(nlogn), O(1)> implementation of RMQ."<<endl;
+	cout<<"ALLPAIRS_FPAIR -> uses the fpairs heuristic from the AllPairs problem to	answer queries of the beginnings problem"<<endl;
 	exit(0);
 }
 
 int main(int argc, char* argv[])
 {
-	bool use_rdataset = false;
-	bool use_rmqH = false;
+	int type_h, outsize;
 
 	if (argc < 2) {
 			help();
 	}
 
-	if (strcmp(argv[1], "RANDOM_DATASET") == 0)	use_rdataset = true;
-	else if (strcmp(argv[1], "REAL_DATASET") == 0) use_rdataset = false;
-	else help();
-	
-	if (strcmp(argv[2], "RMQ_H") == 0) use_rmqH = true;
-	else if (strcmp(argv[2], "FPAIR_H") == 0) use_rmqH = false;
+	if (strcmp(argv[1], "OUTSIZE_SMALL") == 0) outsize = 0;
+	else if (strcmp(argv[1], "OUTSIZE_LARGE") == 0) outsize = 1;
 	else help();
 
+	if (strcmp(argv[2], "RMQ_BUCKET") == 0) type_h = RMQ_BUCKET;
+	else if (strcmp(argv[2], "FPAIR") == 0) type_h = FPAIR;
+	else if (strcmp(argv[2], "ALLPAIRS_FPAIR") == 0) type_h = ALLPAIRS_FPAIR;
+	else if (strcmp(argv[2], "RMQ_ST") == 0) type_h = RMQ_ST;
+	else help();
 
-	if (use_rdataset){
-			DataHandler<int> *data_set = new RandomDataHandler<int>();
-			data_set->load_dataset("/home/cvalentim/Mestrado/research/data_structures/instances/random/");
-			
-			Heuristic<int> *h;
-			if (use_rmqH)
-					h = new RmqBased<int>();
-			else
-				  h = new RangeList<int>();
-			Executer<int> *env = new Executer<int>();
-			// execute 30 queries and repeat each query five times
-			env->exec(data_set, h, 2, 5);
-	}	
-	else{
-			DataHandler<double> *data_set = new RealDataHandler<double>();
-			data_set->load_dataset("/home/cvalentim/Mestrado/research/data_structures/instances/real_data/normal/");
+	DataHandler<double> *data_set = new RealDataHandler<double>();
+	data_set->load_dataset("/home/cvalentim/Mestrado/research/data_structures/instances/real_data/reversed/");
 
-			Heuristic<double> *h;
-			if (use_rmqH)
-				h = new RmqBased<double>;
-			else
-				h = new RangeList<double>();
-			Executer<double> *env = new Executer<double>();
-			// execute 30 queries and repeat each query five times
-			env->exec(data_set, h, 12, 5);
+	Heuristic<double> *h;
+	switch (type_h){
+		case 0:
+			h = new RmqBased<double>(new RMQBucket<double>(RMQTypes_t::MAX()));
+			break;
+		case 1:
+			h = new RangeList<double>();
+			break;
+		case 2:
+			h = new AllPairsFPairs<double>();
+			break;
+		case 3:
+			h = new RmqBased<double>(new RMQSt<double>(RMQTypes_t::MAX()));
+			break;
 	}
+	Executer<double> *env = new Executer<double>();
+	// execute 15 queries and repeat each query five times
+	env->exec(data_set, h, 15, 5, outsize);
 	return 0;
 }
